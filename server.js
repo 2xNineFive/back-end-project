0e6e8e388dcfd865bec441616afcc69d2d3fc20d
Frankie Molina <officialgoldminded@gmail.com>
2023-04-22 07:53:33 -0700

    create server

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 // server code here
 
+// importing express
+const express = require('express')
+
+// caching express to get access to express methods
+const server = express();
 
 
+// heartbeat endpoint
+server.get('/heartbeat', (req, res) => {
+    res.json({
+        "is": "working"
+    })
+})
+
+
+// listening at port 8080
+server.listen(8080, () => console.log("The server is running at port 8080"));
