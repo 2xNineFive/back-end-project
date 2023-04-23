bcbd4e0bffb0b227ffca1db5600aae704b99f58f
Frankie Molina <officialgoldminded@gmail.com>
2023-04-23 10:41:28 -0700

    add style to form and add render function and reset

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 // alert("Hello World!");
 
 const form = document.getElementById("form");
+const credscontainer = form.querySelector('#credentials-container')
 
 function stringifyFormData(fd) {
   const data = {};
@@ -15,6 +16,22 @@ const handleSubmit = (e) => {
   const data = new FormData(e.target);
   const stringified = stringifyFormData(data);
   console.log(stringified);
+  renderform()
 };
 
+
+renderform();
 form.addEventListener("submit", handleSubmit);
+
+function renderform() {
+  const html = `
+    <div class="input-field">
+        <input type="text" name="username" id="username" placeholder="Enter Username">
+    </div>
+    <div class="input-field">
+        <input type="password" name="password" id="password" placeholder="Enter Password">
+    </div>
+    <input type="submit" value="LogIn">
+    `;
+    credscontainer.innerHTML = html;
+}
