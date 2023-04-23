4d0453f7eca092397868d0ab8e764cf944181ff3
Frankie Molina <officialgoldminded@gmail.com>
2023-04-23 15:51:24 -0700

    isolate logic related to /login

--- public/main.js
+++ public/main.js
@@ -1,32 +1,32 @@
 // alert("Hello World!");
 
-const form = document.getElementById("form");
-const credscontainer = form.querySelector('#credentials-container')
+if (window.location.pathname === "/login") {
+  const form = document.getElementById("form");
+  const credscontainer = form.querySelector("#credentials-container");
 
-function stringifyFormData(fd) {
-  const data = {};
-  for (let key of fd.keys()) {
-    data[key] = fd.get(key);
+  function stringifyFormData(fd) {
+    const data = {};
+    for (let key of fd.keys()) {
+      data[key] = fd.get(key);
+    }
+    return JSON.stringify(data, null, 4);
   }
-  return JSON.stringify(data, null, 4);
-}
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  const data = new FormData(e.target);
-  const stringified = stringifyFormData(data);
-  // console.log(stringified);
-  const response = await doLogin(stringified);
-  location.href = response.redirectTo
-  console.log(`The user is logged in: ${response.isAuthenticated}`)
-};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const stringified = stringifyFormData(data);
+    // console.log(stringified);
+    const response = await doLogin(stringified);
+    location.href = response.redirectTo;
+    console.log(`The user is logged in: ${response.isAuthenticated}`);
+  };
 
+  renderform();
+  form.addEventListener("submit", handleSubmit);
 
-renderform();
-form.addEventListener("submit", handleSubmit);
-
-function renderform() {
-  const html = `
+  function renderform() {
+    const html = `
     <div class="input-field">
         <input type="text" name="username" id="username" placeholder="Enter Username">
     </div>
@@ -36,16 +36,17 @@ function renderform() {
     <input type="submit" value="LogIn">
     `;
     credscontainer.innerHTML = html;
-}
+  }
 
-async function doLogin (body) {
-  const data = await fetch('/login', {
-    body, 
-    headers: {
-      'Content-Type': "application/json"
-    },
-    method: 'POST'
-  }) 
-  const response = await data.json();
-  return response;
-} 
+  async function doLogin(body) {
+    const data = await fetch("/login", {
+      body,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+    const response = await data.json();
+    return response;
+  }
+}
